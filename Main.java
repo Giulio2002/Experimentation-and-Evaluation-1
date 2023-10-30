088887907a0ce4cc090bac2ccb68871aa65b2a7f
Giulio <giulio.rebuffo@gmail.com>
2023-10-30 14:49:34 +0100

    save

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.lang.management;
+
+public class Main {
+    public static void main(String[] args) {
+        // Two cases: small array (10 elements) and big array (100'000 elements)
+    }
+
+
+    public execute()
+}
